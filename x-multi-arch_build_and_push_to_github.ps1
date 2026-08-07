3add2a520f405d1fc48bccec917ce308fcf4e734
fkrenn12 <fkrenn@nanet.at>
2026-08-07 09:15:09 +0200

    Add script for multi-arch Docker build and push
    This script enables building and pushing multi-architecture Docker images for amd64 and arm64 platforms. It simplifies deployment processes by automating the image build and push to GitHub.

--- x-multi-arch_build_and_push_to_github.ps1
+++
@@ -1,3 +0,0 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t fkrenn12/loxone-mqtt-bridge:latest --push .
-# docker buildx build --platform linux/amd64,linux/arm64 -t fkrenn12/loxone-mqtt-bridge:latest --no-cache --push .
-
