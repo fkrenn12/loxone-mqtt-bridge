80830cbc7808b0d19e5a4c8db93e42e6e80b7be7
fkrenn12 <fkrenn@nanet.at>
2026-08-14 11:46:37 +0200

    Refactor Zigbee service logic for improved value handling
    Updated brightness scaling and clamped maximum values for color properties to improve functionality. Refactored value mapping logic with additional model checks and fixed issues in property handling. Minor cleanup of redundant code for better readability.

--- src/zigbee.py
+++ src/zigbee.py
@@ -7,17 +7,17 @@ RGB_WHITE = [255, 255, 255]
 RGB_BLACK = [0, 0, 0]
 
 services_database = {
-        # "turn_on": {},
-        # "turn_off": {},
-        "state": lambda value: {"state": value},
-        "brightness": lambda value: {"brightness": value},
-        "color_temp": lambda value: {"color_temp": value},
-        "rgb": lambda value: {"color": {"rgb": ",".join(map(str, loxone_color_code2rgb(loxone_color_code=value)))},
-                              "brightness": loxone_color_code_maxvalue(loxone_color_code=value),
-                              },
-        "color": lambda value: {"color": {"rgb": ",".join(map(str, loxone_color_code2rgb(loxone_color_code=value)))},
-                                "brightness": loxone_color_code_maxvalue(loxone_color_code=value),
-                                }
+    # "turn_on": {},
+    # "turn_off": {},
+    "state": lambda value: {"state": value},
+    "brightness": lambda value: {"brightness": int(value * 2.54)},
+    "color_temp": lambda value: {"color_temp": value},
+    "rgb": lambda value: {"color": value,
+                          "brightness": min(max(value.values()), 254)
+                          },
+    "color": lambda value: {"color": {"rgb": ",".join(map(str, loxone_color_code2rgb(loxone_color_code=value)))},
+                            "brightness": min(loxone_color_code_maxvalue(loxone_color_code=value), 254)
+                            }
 }
 
 property_mappings = {"color_temp_percent": "color_temp"}
@@ -29,15 +29,16 @@ def handle_zigbee_service(device, prop, value):
     model = next((m for m in config.definitions["definitions"] if m["definition(Zigbee2MQTT)"] == model_name), None)
     prop = model["default_expose"] if prop is None else prop
     # property limits and scaling
-    limits = model.get("limits", {})
-    if prop in limits and is_castable_to_numeric(value):
-        in_min = limits[prop].get("in_min", 0)
-        in_max = limits[prop].get("in_max", 100)
-        out_min = limits[prop].get("out_min", 0)
-        out_max = limits[prop].get("out_max", 100)
-        value = int(scale_and_clamp(int(value), in_min, in_max, out_min, out_max))
+    if model:
+        limits = model.get("limits", {})
+        if prop in limits and is_castable_to_numeric(value):
+            in_min = limits[prop].get("in_min", 0)
+            in_max = limits[prop].get("in_max", 100)
+            out_min = limits[prop].get("out_min", 0)
+            out_max = limits[prop].get("out_max", 100)
+            value = int(scale_and_clamp(int(value), in_min, in_max, out_min, out_max))
 
-    value = apply_value_mapping(model, prop, value)
+        value = apply_value_mapping(model, prop, value)
     value = cast_to_numeric(value)
 
     # convert true/True and false/False to boolean
@@ -49,7 +50,8 @@ def handle_zigbee_service(device, prop, value):
         pass
 
     try:
-        return services_database[prop](value)
-    except:
+        x = services_database[prop](value)
+        # print(x)
+        return x
+    except Exception as e:
         return {}
-
